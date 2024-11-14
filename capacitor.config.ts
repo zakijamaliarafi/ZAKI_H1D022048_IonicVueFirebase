bb19431ecdcdc2e07f70bfcb22625d4191e1e29b
zakijamaliarafi <zakijamaliarafi@gmail.com>
2024-11-14 09:52:54 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-firebase',
+  webDir: 'dist'
+};
+
+export default config;
